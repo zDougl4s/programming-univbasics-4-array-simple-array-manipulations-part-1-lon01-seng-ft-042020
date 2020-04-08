86552272c2c8c793ead9c107d7eba89a18455343
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-08 19:57:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,15 @@ end
 def using_unshift(array,string)
   array.unshift(string)
 end
+
+def unsing_pop (array,string)
+  array.pop(string)
+end
+
+def pop_with_args(array,string)
+
+end
+
+def using_shift(array,string)
+  array.shift(string)
+end
